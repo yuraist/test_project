2f21f39db5668ed96de34f5bddfb97bf0e0811a3
Yuraist <yura.ist@icloud.com>
2018-11-09 13:46:21 +0300

    Changed regexs for the access_task and finish_task urls

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     url(r'^get_open_tasks/$', views.GetOpenTasksView.as_view(), name='get_open_tasks'),
     url(r'^get_finished_tasks/$', views.GetFinishedTasksView.as_view(), name='get_finished_tasks'),
     url(r'^get_in_progress_tasks/$', views.GetInProgressTasksView.as_view(), name='get_in_progress_tasks'),
-    url(r'^accept_task/$', views.AcceptTaskView.as_view(), name='accept_task'),
-    url(r'^finish_task/$', views.FinishTaskView.as_view(), name='finish_task'),
+    url(r'^accept_task/(?P<id>[0-9]+)/$', views.AcceptTaskView.as_view(), name='accept_task'),
+    url(r'^finish_task/(?P<id>[0-9]+)/$', views.FinishTaskView.as_view(), name='finish_task'),
 ]
