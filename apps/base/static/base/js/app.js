8b611963d98e97590cd6004ae3bbd5185b0d355e
Yuraist <yura.ist@icloud.com>
2018-11-09 17:33:58 +0300

    Added functions for loading selected tasks

--- apps/base/static/base/js/app.js
+++ apps/base/static/base/js/app.js
@@ -1,5 +1,44 @@
 function changeMenuActiveTab(button) {
     $('.nav-tabs li').removeClass('active');
     $(button).parent().addClass('active');
-    console.log('WORK!');
+
+    const selectedTab = $(button).text();
+    switch (selectedTab) {
+        case 'Все заказы':
+            loadAllTasks();
+            break;
+        case 'Ожидающие':
+            loadOpenTasks();
+            break;
+        case 'Принятые':
+            loadInProgressTasks();
+            break;
+        case 'Выполненные':
+            loadFinishedTasks();
+            break;
+    }
+}
+
+function loadAllTasks() {
+    console.log('load all tasks');
+}
+
+function loadOpenTasks() {
+    console.log('load open tasks');
+}
+
+function loadInProgressTasks() {
+    console.log('load in progress tasks');
+}
+
+function loadFinishedTasks() {
+    console.log('load finished tasks');
+}
+
+function acceptTask() {
+    console.log('accept');
+}
+
+function finishTask() {
+    console.log('finish');
 }
